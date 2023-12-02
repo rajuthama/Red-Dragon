21d23dac439061796b6ff138a99b2c608328778a
rajuthama <151140523+rajuthama@users.noreply.github.com>
2023-12-02 13:09:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -47,7 +47,7 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
+global.owner = ['94729450909']
 global.ownertag = '+380944814219' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
